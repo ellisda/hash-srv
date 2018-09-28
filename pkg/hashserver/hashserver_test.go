987faf3c5a50097106ec25bb0fdcd2c72cdb55e8
David Ellis <ellisda@gmail.com>
2018-09-28 16:28:37 -0600

    refactor tests

--- pkg/hashserver/hashserver_test.go
+++ pkg/hashserver/hashserver_test.go
@@ -11,33 +11,35 @@ import (
 
 var hashVal = [64]byte{0, 1, 2, 3, 4, 5, 6, 7}
 
-func getTestServer() (*HashServer, *httptest.ResponseRecorder) {
-	return NewHashServer(8080), httptest.NewRecorder()
+func getTestServer() *HashServer {
+	return NewHashServer(8080)
+}
+
+func execHandler(t *testing.T, h func(http.ResponseWriter, *http.Request), req *http.Request, expectedStatus int,
+) *httptest.ResponseRecorder {
+	recorder := httptest.NewRecorder()
+	http.HandlerFunc(h).ServeHTTP(recorder, req)
+	if recorder.Code != expectedStatus {
+		t.Fatalf("Expected http.Handler to return Status %d, got %d", expectedStatus, recorder.Code)
+	}
+	return recorder
 }
 
 func TestHashRequest(t *testing.T) {
-	srv, recorder := getTestServer()
+	srv := getTestServer()
 
 	req, err := http.NewRequest("POST", "/hash", strings.NewReader("password=foo"))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	http.HandlerFunc(srv.hashRequest).ServeHTTP(recorder, req)
-	if recorder.Code != 202 {
-		t.Fatal("Expected 202 Status")
-	}
+	recorder := execHandler(t, srv.hashRequest, req, 202)
 	body := recorder.Body.String()
 	if body != "1" {
 		t.Fatal("Expected response body to start at 1")
 	}
 
-	recorder = httptest.NewRecorder()
-	http.HandlerFunc(srv.hashRequest).ServeHTTP(recorder, req)
-	if recorder.Code != 202 {
-		t.Fatal("Expected 202 Status")
-	}
+	recorder = execHandler(t, srv.hashRequest, req, 202)
 	body = recorder.Body.String()
 	if body != "2" {
 		t.Fatalf("Expected response body to continue at 2")
@@ -45,79 +47,54 @@ func TestHashRequest(t *testing.T) {
 }
 
 func TestHashRequestRejected(t *testing.T) {
-	srv, recorder := getTestServer()
+	srv := getTestServer()
 
 	emptyReq, err := http.NewRequest("POST", "/hash", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	http.HandlerFunc(srv.hashRequest).ServeHTTP(recorder, emptyReq)
-	if recorder.Code != 400 {
-		t.Fatal("Expected 400 Status for empty POST body")
-	}
+	execHandler(t, srv.hashRequest, emptyReq, 400)
 
 	badReq, err := http.NewRequest("POST", "/hash", strings.NewReader("foo=bar"))
 	badReq.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	http.HandlerFunc(srv.hashRequest).ServeHTTP(recorder, badReq)
-	if recorder.Code != 400 {
-		t.Fatal("Expected 400 Status for POST body without 'password' form value")
-	}
+	execHandler(t, srv.hashRequest, badReq, 400)
 }
 
 func TestGetHashRequest(t *testing.T) {
-	srv, recorder := getTestServer()
+	srv := getTestServer()
 
 	req, err := http.NewRequest("GET", "/hash/1", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	http.HandlerFunc(srv.getHash).ServeHTTP(recorder, req)
-	if recorder.Code != 400 {
-		t.Error("Expected HTTP Response Status 400 before hash key exists")
-	}
+	execHandler(t, srv.getHash, req, 400)
 
-	recorder = httptest.NewRecorder()
 	srv.hashes[1] = hashVal
-	http.HandlerFunc(srv.getHash).ServeHTTP(recorder, req)
-	if recorder.Code != 200 {
-		t.Error("Expected HTTP Response Status 200 after hash key exists")
-	}
+	recorder := execHandler(t, srv.getHash, req, 200)
 	if recorder.Body.String() != base64.StdEncoding.EncodeToString(hashVal[:]) {
 		t.Error("Expected HTTP Response to match base64 hashed value")
 	}
 }
 
 func TestGetStats(t *testing.T) {
-	srv, recorder := getTestServer()
+	srv := getTestServer()
 
 	req, err := http.NewRequest("GET", "/stats", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	http.HandlerFunc(srv.getStats).ServeHTTP(recorder, req)
-	if recorder.Code != 200 {
-		t.Error("Expected HTTP Response Status 200")
-	}
+	recorder := execHandler(t, srv.getStats, req, 200)
 	if recorder.Body.String() != "{\"total\":0,\"average\":0}" {
 		t.Error("Stats produced unexpected content")
 	}
 
 	srv.stats.NumProcessed = 1
-	recorder = httptest.NewRecorder()
-	http.HandlerFunc(srv.getStats).ServeHTTP(recorder, req)
-	if recorder.Code != 200 {
-		t.Error("Expected HTTP Response Status 200")
-	}
+	recorder = execHandler(t, srv.getStats, req, 200)
 	var stats requestStats
-	bytes := recorder.Body.Bytes()
-	s := string(bytes)
-	t.Log(s)
-	json.Unmarshal(bytes, &stats)
+	json.Unmarshal(recorder.Body.Bytes(), &stats)
 	if stats.NumProcessed != 1 {
 		t.Error("Stats produced unexpected content")
 	}
